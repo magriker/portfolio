ed8438fb511c903d8498c4bdca565af48d4e32f0
magriker <magken7@gmail.com>
2025-01-25 21:27:45 +0900

    commmit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,52 @@
 import "../App.css";
+import "../Footer.css";
+import Label from "./Label";
 
 const Footer = () => {
   return (
     <footer>
+      <div className="contact-box">
+        <Label size="l" bold>
+          Let's Talk
+          <br />
+        </Label>
+        <Label size="l">
+          charlotte@traumfabrikat.de
+          <br />
+          +818075853392
+        </Label>
+      </div>
       <div className="snsAndportfolio-box">
         <div className="socialMeia-icons">
-          <img src=" public\LinkedIn_Icon.png " alt="linkedinicon"></img>
-          <img src=" public\Instagram_icon.png " alt="Instagramicon"></img>
-          <img src=" public\Github_icon.png" alt="Githubicon"></img>
-          <img src=" public\Figma_icon.png " alt="Figmaicon"></img>
+          <img
+            src=" public\LinkedIn_Icon.png "
+            alt="linkedinicon"
+            className="icon"
+          ></img>
+          <img
+            src=" public\Instagram_icon.png "
+            alt="Instagramicon"
+            className="icon"
+          ></img>
+          <img
+            src=" public\Github_icon.png"
+            alt="Githubicon"
+            className="icon"
+          ></img>
+          <img
+            src=" public\Figma_icon.png "
+            alt="Figmaicon"
+            className="icon"
+          ></img>
         </div>
         <div className="portfolio-box">
           <img src="public\qrcode.png" alt="qrcode" className="qrpic" />
-          <p className="portfoliotext-box">download my portfolio</p>
+          <div className="portfoliotext-box">
+            <Label size="s" bold>
+              download my portfolio
+            </Label>
+          </div>
         </div>
-      </div>
-      <div className="contact-box">
-        <p className="catchphrase">Let's Talk</p>
-        <p className="email">charlotte@traumfabrikat.de</p>
-        <p className="phon-number">+818075853392</p>
       </div>
     </footer>
   );
