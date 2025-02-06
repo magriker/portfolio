4289b6e7385ae97bedc678c8779f5a9b522c0946
magriker <magken7@gmail.com>
2025-02-07 00:58:03 +0900

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,32 @@ import "../App.css";
 import Label from "./Label";
 import Tab from "./Tab";
 import "../Header.css";
+import { createClient } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const supabaseUrl = "https://cvlwnazscqnftpfwhsac.supabase.co";
+  const supabaseKey =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2bHduYXpzY3FuZnRwZndoc2FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg4NDY3MTgsImV4cCI6MjA1NDQyMjcxOH0.VmjcDRP04_5RklbY8DfCcWIzRMPFGlklQlRlJTdALoY";
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [producs, setProducts] = useState([] as any[]);
+
+  async function fetchProducts() {
+    const { data, error } = await supabase
+      .from("Producs")
+      .select("*")
+      .order("created_at", { ascending: false });
+    if (!error) setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(producs[0]?.main_img_url);
+
   return (
     <div className="header-container">
       <div className="intro-box">
@@ -30,6 +54,7 @@ const Header = () => {
           ></Tab>
         </nav>
         <div className="language-box"></div>
+        <img src={producs[0]?.main_img_url} alt="" />
       </div>
     </div>
   );
