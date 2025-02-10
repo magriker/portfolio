dfdc4927860eb643609082abe537ab67d0206e52
magriker <magken7@gmail.com>
2025-02-11 01:43:27 +0900

    commit

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -8,10 +8,6 @@ const Details = () => {
   const location = useLocation();
   const product = location.state.product;
   const productId = product.id;
-  console.log(product);
-
-  console.log(productId);
-
   const navigate = useNavigate();
 
   const handleBack = () => {
@@ -21,27 +17,18 @@ const Details = () => {
     <>
       <div className="backbutton">
         <a onClick={() => handleBack()}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="arrow"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
-            />
-          </svg>
-
-          <Label size="s"> back</Label>
+          <span className="arrow">&larr;</span>
+          <Label size="s" bold hover>
+            {" "}
+            back
+          </Label>
         </a>
       </div>
       <div className="detail-box">
         <div className="product-name">
-          <h1>Product{productId}</h1>
+          <Label size="l" bold>
+            {product.name}
+          </Label>
         </div>
         <div className="product-pictures"></div>
         <div className="detailed-explanation">{product.description}</div>
